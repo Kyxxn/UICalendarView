b485da4e166af2a008ff5f04868d977d689f18e6
Kyxxn <gywns626@naver.com>
2024-08-13 18:19:55 +0900

    멀티 클릭을 싱글 클릭으로 구현해서 선택한 날짜 해제가능

--- UICalendarViewStudy/ViewController.swift
+++ UICalendarViewStudy/ViewController.swift
@@ -33,6 +33,13 @@ class ViewController: UIViewController {
         setButton()
         setSelectionBehavior()
     }
+    
+    /// 하나만 클릭될 수 있게, 만약 여러 개 선택하고 싶으면 MultiDateDelegate
+    func setSelectionBehavior() {
+        // UICalendarSelectionSingleDate(delegate: self)
+        let selectionDelegate = UICalendarSelectionMultiDate(delegate: self)
+        calendarView.selectionBehavior = selectionDelegate
+    }
 }
 
 // MARK: UICalendarViewDelegate 프로토콜 메소드
@@ -52,15 +59,10 @@ extension ViewController: UICalendarViewDelegate {
     }
 }
 
-
 // MARK: 하나만 선택했을 때, 어떤 동작 할래??
 extension ViewController: UICalendarSelectionSingleDateDelegate {
-    /// 하나만 클릭될 수 있게, 만약 여러 개 선택하고 싶으면 MultiDateDelegate
-    func setSelectionBehavior() {
-        let singleSelection = UICalendarSelectionSingleDate(delegate: self)
-        calendarView.selectionBehavior = singleSelection
-    }
     
+    /// 날짜가 하나 클릭 됐을 때 동작
     func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
         enableButton()
         if let date = dateComponents?.date {
@@ -69,11 +71,39 @@ extension ViewController: UICalendarSelectionSingleDateDelegate {
             print("선택된 날짜: \(formattedDate)")
         }
     }
-    
-    private func enableButton() {
-        myButton.isEnabled = true
-        myButton.backgroundColor = .systemOrange
+}
+
+/// multiDateSelection(_:didSelectDate:): 사용자가 날짜를 선택할 때 호출되며, 선택된 날짜에 대한 작업을 처리할 수 있습니다.
+/// multiDateSelection(_:didDeselectDate:): 사용자가 날짜를 선택 해제할 때 호출되며, 해제된 날짜에 대한 작업을 처리할 수 있습니다.
+/// multiDateSelection(_:canSelectDate:): 특정 날짜가 선택 가능한지 여부를 결정하는 메소드로, 선택을 제한할 수 있습니다.
+/// multiDateSelection(_:canDeselectDate:): 특정 날짜가 선택 해제 가능한지 여부를 결정하는 메소드로, 해제를 제한할 수 있습니다.
+
+extension ViewController: UICalendarSelectionMultiDateDelegate {
+    func multiDateSelection(_ selection: UICalendarSelectionMultiDate, didSelectDate dateComponents: DateComponents) {
+        if let date = dateComponents.date {
+            // 이미 선택된 날짜가 있고, 그 날짜와 동일한 날짜가 다시 클릭되면 해제
+            if let selectedDate = selectedDate, selectedDate == date {
+                selection.setSelectedDates([], animated: false)
+                self.selectedDate = nil
+                print("선택 해제된 날짜: \(selectedDate)")
+                return
+            } else {
+                // 다른 날짜를 클릭하면 이전 날짜를 해제하고 새 날짜를 선택
+                selection.setSelectedDates([dateComponents], animated: true)
+            }
+            
+            self.selectedDate = date // 선택된 날짜를 저장
+            enableButton()
+            let formattedDate = formatDate(date)
+            print("선택된 날짜: \(formattedDate)")
+        }
     }
+    
+    func multiDateSelection(_ selection: UICalendarSelectionMultiDate, didDeselectDate dateComponents: DateComponents) {
+        print("선택 해제된 날짜: \(dateComponents.date!)")
+        disableButton()
+    }
+
 }
 
 // MARK: 날짜만 뽑아내기
@@ -111,5 +141,18 @@ extension ViewController {
             $0.trailing.equalTo(view.snp.trailing).offset(-18)
             $0.height.equalTo(56)
         }
+    }
+}
+
+// MARK: 버튼 활성/비활성 세팅
+extension ViewController {
+    private func enableButton() {
+        myButton.isEnabled = true
+        myButton.backgroundColor = .systemOrange
+    }
+    
+    private func disableButton() {
+        myButton.isEnabled = false
+        myButton.backgroundColor = .systemGray5
     }
 }
