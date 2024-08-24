9c90d508117a343532a2df982f8900a9184c0ee1
Kyxxn <gywns626@naver.com>
2024-08-24 22:29:32 +0900

    feat: SelectedDateType 달력 선택 종류 열거형 추가

---
+++ UICalendarViewStudy/Source/FSCalendar/MutliSelectRange/MultiSelectRangeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SelectRangeFSCalnderViewController.swift
+//  UICalendarViewStudy
+//
+//  Created by 박효준 on 8/24/24.
+//
+
+import UIKit
+import FSCalendar
+import SnapKit
+import Then
+
+enum SelectedDateType {
+    case singleDate  // 단일 날짜 선택
+    case firstDate   // 여러 날짜 선택 시, 시작일
+    case middleDate  // 시작-종료를 제외한 중간 날짜
+    case lastDate    // 여러 날짜 선택 시, 종료일
+    case notSelected // 선택되지 않은 날짜
+}
+
+class MultiSelectRangeViewController: UIViewController {
+    let calendarView = FSCalendar().then {
+        $0.scrollDirection = .horizontal
+        $0.allowsMultipleSelection = true
+        
+    }
+}
